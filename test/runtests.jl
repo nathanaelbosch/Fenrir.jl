4842d2a4b8d0a84d53fd2b98544c1a8d325d6f38
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-11-01 17:30:23 +0100

    Add Aqua and JET to the testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
 using Fenrir
 using Test
-using SafeTestsets
+using SafeTestsets, Aqua, JET
 
 @testset "Fenrir.jl" begin
     @safetestset "fenrir_nll" begin
         include("fenrir_nll.jl")
+    end
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(Fenrir, ambiguities=false)
+    end
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(Fenrir; target_defined_modules=true)
     end
 end
