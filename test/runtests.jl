cf1ff95bf8ce2df32f4913169008ad5ad403927d
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-11-12 13:13:17 +0100

    Check CI and fix tests (#29)
    * Dummy change in the README, just to get the CI going
    
    * Add test groups
    
    * remove the dummy change again
    
    * Only run JET if on 1.9 (for some reason I don't quite understand tbh)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,23 @@ using Fenrir
 using Test
 using SafeTestsets, Aqua, JET
 
+const GROUP = get(ENV, "GROUP", "All")
+
 @testset "Fenrir.jl" begin
-    @safetestset "fenrir_nll" begin
-        include("fenrir_nll.jl")
+    if GROUP == "All" || GROUP == "Downstream"
+        @safetestset "fenrir_nll" begin
+            include("fenrir_nll.jl")
+        end
     end
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(Fenrir, ambiguities=false)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(Fenrir; target_defined_modules=true)
+
+    if GROUP == "All"
+        @testset "Code quality (Aqua.jl)" begin
+            Aqua.test_all(Fenrir, ambiguities=false)
+        end
+        if VERSION >= v"1.9"
+            @testset "Code linting (JET.jl)" begin
+                JET.test_package(Fenrir; target_defined_modules=true)
+            end
+        end
     end
 end
