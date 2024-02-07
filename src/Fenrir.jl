d2b239e28eebe80b1108d0d0f67084d8d824b3c3
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2024-02-07 16:06:12 +0100

    Add a deprecation warning that is shown whenever Fenrir is loaded

--- src/Fenrir.jl
+++ src/Fenrir.jl
@@ -10,4 +10,14 @@ using SimpleUnPack
 include("likelihood.jl")
 export fenrir_nll
 
+function __init__()
+    # Deprecation warning: This package will not be maintained in the future.
+    @warn("""\n
+        # Deprecation warning: This package will not be maintained in the future.
+        The `fenrir_nll` function implemented in this package is now implemented in
+        ProbNumDiffEq.jl, together with other data likelihood functions. So, instead of
+        using Fenrir.jl, use ProbNumDiffEq.jl.
+    """)
+end
+
 end
